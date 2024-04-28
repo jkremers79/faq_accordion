35a9c1a11363bc28965dde60c2b8d9bcee352584
Jorn Kremers <j.kremers@proton.me>
2024-04-28 12:44:17 +0200

    small adjustment in js code

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const buttons = document.querySelectorAll(".faq-button");
+const faqSections = document.querySelectorAll(".faq-section");
 
 buttons.forEach((btn, index) => {
     btn.addEventListener("click", () => showFAQ(index));
 })
 
 function showFAQ(n) {
-    const elements = document.querySelectorAll(".faq-section");
-    const clickedElement = elements[n].classList;
-
+    const clickedElement = faqSections[n].classList;
     clickedElement.toggle("active");
 }
